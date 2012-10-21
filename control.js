d7d22e8f589f2a0bc8b68f9638aadfcae8186c08
Matt <matt.pardee@gmail.com>
2012-10-21 13:40:19 -0400

    Added issue bang, rearranged things

--- control.js
+++ control.js
@@ -1,37 +1,53 @@
-var botID = "!c9status";
-var users = {bmatusiak:"brad123"}
-if(!process.users)
-	process.users = {}
-	
-var AuthUsers = process.users 
-var controler = {}
+var nodegh = require("node-github");
 
-controler.status = function(passObj,messageElements){
+if(!process.users)
+	process.users = {};
+	
+var controller = {};
+
+// Generic send method
+controller.say = function(passObj, message) {
+    // Channel
+    if(passObj.client.nick !== passObj.to)
+		passObj.client.say(passObj.to,message);
+	// PM
+    else
+		passObj.client.say(passObj.from,message);
+};
+
+controller.status = function(passObj, messageElements) {
 	var request = require("request");
     request('http://status.c9.io/', function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-  		
+        if (!error && response.statusCode == 200) {
 			var jsdom = require('jsdom').jsdom;
-    		var window   = jsdom(body).createWindow();
-    		var message = window.document.getElementById("status-indicator").innerHTML+" ( http://status.c9.io/ )";
-    		if(passObj.client.nick !== passObj.to){//Channel
-				passObj.client.say(passObj.to,message)
-			}else{//PM
-				passObj.client.say(passObj.from,message)
-			}
-  		}
-	})
-}
+            var window  = jsdom(body).createWindow();
+            var message = window.document.getElementById("status-indicator").innerHTML + " ( http://status.c9.io/ )";
+            controller.say(passObj, message);
+        }
+	});
+};
 
-module.exports.message = function(client,from,to,message){
-	var passObj = {client:client,from:from,to:to,message:message}
+controller.search = function(passObj, message) {
+    var searchPhrase = message.substr(8);
+};
+
+controller.issue = function(passObj, messageElements) {
+    controller.say(passObj, "https://github.com/ajaxorg/cloud9/issues/new");
+};
+
+module.exports.message = function(client, from, to, message){
+	var passObj = {client:client,from:from,to:to,message:message};
 	console.log({from:from,to:to,message:message});
 	var messageElements = message.split(" ");
-	//console.log(passObj);
-	if(messageElements[0] === botID){
-		var chatControler = controler.status
-		if(chatControler){
-			chatControler(passObj,messageElements)
-		}
-	}
-}
+    switch(messageElements[0]) {
+        case "!c9status":
+            controller.status(passObj, messageElements);
+            break;
+        case "!issue":
+            controller.issue(passObj, messageElements);
+            break;
+        case "!search":
+            controller.search(passObj, message);
+            break;
+    }
+};
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var irc = require('irc');
-var client = new irc.Client('irc.freenode.net', 'c9status_bot', {
+var client = new irc.Client('irc.freenode.net', 'c9bot', {
     channels: ['#cloud9ide']
 });
 
 client.addListener('message', function (from, to, message) {
-    var filename = __dirname + "/control.js"
+    var filename = __dirname + "/control.js";
 	require(filename).message(client,from,to,message);
     delete require.cache[filename];
 });
